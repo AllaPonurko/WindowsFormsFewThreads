4f84fc1173be6e8eb31e8e13d88c718977cb19e7
Alla Ponurko <ponurkoalla264@gmail.com>
2022-09-12 02:50:15 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsFewThreads/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace WindowsFormsFewThreads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.lstNumber = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lstPrimeNumber = new System.Windows.Forms.ListBox();
+            this.lstEndOnSeven = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Генератор случайных чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Нижняя граница";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Верхняя граница";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(111, 71);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 3;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(111, 98);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 4;
+            // 
+            // lstNumber
+            // 
+            this.lstNumber.FormattingEnabled = true;
+            this.lstNumber.Location = new System.Drawing.Point(16, 137);
+            this.lstNumber.Name = "lstNumber";
+            this.lstNumber.Size = new System.Drawing.Size(195, 251);
+            this.lstNumber.TabIndex = 5;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(16, 408);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(100, 23);
+            this.btnShow.TabIndex = 6;
+            this.btnShow.Text = "Генерировать";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lstPrimeNumber
+            // 
+            this.lstPrimeNumber.FormattingEnabled = true;
+            this.lstPrimeNumber.Location = new System.Drawing.Point(279, 137);
+            this.lstPrimeNumber.Name = "lstPrimeNumber";
+            this.lstPrimeNumber.Size = new System.Drawing.Size(197, 251);
+            this.lstPrimeNumber.TabIndex = 7;
+            // 
+            // lstEndOnSeven
+            // 
+            this.lstEndOnSeven.FormattingEnabled = true;
+            this.lstEndOnSeven.Location = new System.Drawing.Point(508, 137);
+            this.lstEndOnSeven.Name = "lstEndOnSeven";
+            this.lstEndOnSeven.Size = new System.Drawing.Size(185, 251);
+            this.lstEndOnSeven.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(279, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Простые числа";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(508, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Числа , оканчивающиеся на 7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Количество чисел";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(111, 43);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(100, 20);
+            this.txtCount.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstEndOnSeven);
+            this.Controls.Add(this.lstPrimeNumber);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lstNumber);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Множество потоков";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.ListBox lstNumber;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.ListBox lstPrimeNumber;
+        private System.Windows.Forms.ListBox lstEndOnSeven;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCount;
+    }
+}
+
---
+++ WindowsFormsFewThreads/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsFewThreads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            vs = new List<int>();
+            prime = new List<int>();
+            seven = new List<int>();
+        }
+        static private int start;
+        static private int end;
+        static private int count;
+        static private List<int> vs;
+        static private List<int> prime;
+        static private List<int> seven;
+        static private void MethodSeven()
+        {
+            seven.Clear();
+            foreach(var item in vs)
+            {
+                if (item % 10 == 7)
+                    seven.Add(item);
+            }
+        }
+        static private void MethodPrimeNumber()
+        {
+            prime.Clear();
+            foreach(var item in vs)
+            {
+                bool b = true;
+                for (int j = 2; j < item; j++)
+                {
+                    if (item % j == 0 & item % 1 == 0)
+                    {
+                        b = false;
+                    }
+                }
+                if (b) prime.Add(item);
+            }
+        }
+        static private void MethodRandomNum()
+        {
+            vs.Clear();
+            Random random = new Random();
+            for(int i=0;i<count;i++)
+            {if (start < end)
+                { 
+                    vs.Add(random.Next(start, end)); 
+                }
+                else
+                {
+                    vs.Add(random.Next(end, start));
+                }
+            }
+
+        }
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if(txtStart.Text.Length==0||txtEnd.Text.Length==0||txtCount.Text.Length==0)
+            {
+                MessageBox.Show("Нет входящих параметров");
+            }
+            if (txtStart.Text.Length != 0 & txtEnd.Text.Length != 0 & txtCount.Text.Length != 0)
+            {
+                start =Convert.ToInt32(txtStart.Text);
+                end = Convert.ToInt32(txtEnd.Text);
+                count = Convert.ToInt32(txtCount.Text);
+                try
+                { Mutex mutex = new Mutex();
+                    mutex.WaitOne();
+                    lstNumber.Items.Clear();
+                    ThreadStart Start = new ThreadStart(MethodRandomNum);
+                    Thread thread = new Thread(Start);
+                    thread.Start();
+                    Thread.Sleep(1000);
+                    foreach(var item in vs)
+                    {
+                        lstNumber.Items.Add(item);
+                    }
+                    mutex.ReleaseMutex();
+                    mutex.WaitOne();
+                    ThreadStart Start1 = new ThreadStart(MethodPrimeNumber);
+                    Thread thread1 = new Thread(Start1);
+                    thread1.Start();
+                    Thread.Sleep(2000);
+                    lstPrimeNumber.Items.Clear();
+                    foreach(var item in prime)
+                    {
+                        lstPrimeNumber.Items.Add(item);
+                    }
+                    mutex.ReleaseMutex();
+                    mutex.WaitOne();
+                    ThreadStart Start2 = new ThreadStart(MethodSeven);
+                    Thread thread2 = new Thread(Start2);
+                    thread2.Start();
+                    Thread.Sleep(2000);
+                    lstEndOnSeven.Items.Clear();
+                    foreach(var item in seven)
+                    {
+                        lstEndOnSeven.Items.Add(item);
+                    }
+                    mutex.ReleaseMutex();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            
+
+        }
+    }
+}
