50dac5c4c4ae94a0a023ff0bf0b959a168148d97
Alla Ponurko <ponurkoalla264@gmail.com>
2022-09-13 01:56:44 +0300

    111

--- WindowsFormsFewThreads/Form1.cs
+++ WindowsFormsFewThreads/Form1.cs
@@ -16,99 +16,45 @@ namespace WindowsFormsFewThreads
         public Form1()
         {
             InitializeComponent();
-            vs = new List<int>();
-            prime = new List<int>();
-            seven = new List<int>();
         }
-        static private int start;
-        static private int end;
-        static private int count;
-        static private List<int> vs;
-        static private List<int> prime;
-        static private List<int> seven;
-        static private void MethodSeven()
-        {
-            seven.Clear();
-            foreach(var item in vs)
-            {
-                if (item % 10 == 7)
-                    seven.Add(item);
-            }
-        }
-        static private void MethodPrimeNumber()
-        {
-            prime.Clear();
-            foreach(var item in vs)
-            {
-                bool b = true;
-                for (int j = 2; j < item; j++)
-                {
-                    if (item % j == 0 & item % 1 == 0)
-                    {
-                        b = false;
-                    }
-                }
-                if (b) prime.Add(item);
-            }
-        }
-        static private void MethodRandomNum()
-        {
-            vs.Clear();
-            Random random = new Random();
-            for(int i=0;i<count;i++)
-            {if (start < end)
-                { 
-                    vs.Add(random.Next(start, end)); 
-                }
-                else
-                {
-                    vs.Add(random.Next(end, start));
-                }
-            }
-
-        }
+       
         private void btnShow_Click(object sender, EventArgs e)
-        {
+        {MyThreads my = new MyThreads();
             if(txtStart.Text.Length==0||txtEnd.Text.Length==0||txtCount.Text.Length==0)
             {
                 MessageBox.Show("Нет входящих параметров");
             }
             if (txtStart.Text.Length != 0 & txtEnd.Text.Length != 0 & txtCount.Text.Length != 0)
             {
-                start =Convert.ToInt32(txtStart.Text);
-                end = Convert.ToInt32(txtEnd.Text);
-                count = Convert.ToInt32(txtCount.Text);
+                MyThreads.start = Convert.ToInt32(txtStart.Text);
+                MyThreads.end = Convert.ToInt32(txtEnd.Text);
+                MyThreads.count = Convert.ToInt32(txtCount.Text);
                 try
                 { Mutex mutex = new Mutex();
                     mutex.WaitOne();
                     lstNumber.Items.Clear();
-                    ThreadStart Start = new ThreadStart(MethodRandomNum);
-                    Thread thread = new Thread(Start);
-                    thread.Start();
+                    my.T_Random.Start();
                     Thread.Sleep(1000);
-                    foreach(var item in vs)
+                    foreach(var item in MyThreads.vs)
                     {
                         lstNumber.Items.Add(item);
                     }
+                    my.SaveList();
                     mutex.ReleaseMutex();
                     mutex.WaitOne();
-                    ThreadStart Start1 = new ThreadStart(MethodPrimeNumber);
-                    Thread thread1 = new Thread(Start1);
-                    thread1.Start();
+                    my.T_Prime.Start();
                     Thread.Sleep(2000);
                     lstPrimeNumber.Items.Clear();
-                    foreach(var item in prime)
+                    foreach(var item in MyThreads.prime)
                     {
                         lstPrimeNumber.Items.Add(item);
                     }
                     mutex.ReleaseMutex();
                     mutex.WaitOne();
-                    ThreadStart Start2 = new ThreadStart(MethodSeven);
-                    Thread thread2 = new Thread(Start2);
-                    thread2.Start();
+                    my.T_Seven.Start();
                     Thread.Sleep(2000);
                     lstEndOnSeven.Items.Clear();
-                    foreach(var item in seven)
+                    foreach(var item in MyThreads.seven)
                     {
                         lstEndOnSeven.Items.Add(item);
                     }
@@ -119,8 +65,6 @@ namespace WindowsFormsFewThreads
                     MessageBox.Show(ex.Message);
                 }
             }
-            
-
         }
     }
 }
---
+++ WindowsFormsFewThreads/MyThreads.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsFewThreads
+{
+   public class MyThreads
+    {
+        public MyThreads()
+        {
+            vs = new List<int>();
+            prime = new List<int>();
+            seven= new List<int>();
+            T_Random = new Thread(StartRandom);
+            T_Prime = new Thread(StartPrime);
+            T_Seven = new Thread(StartSeven);
+        }
+        public ThreadStart StartRandom = new ThreadStart(MethodRandomNum);
+        public Thread T_Random;
+        public ThreadStart StartPrime = new ThreadStart(MethodPrimeNumber);
+        public Thread T_Prime;
+        public ThreadStart StartSeven = new ThreadStart(MethodSeven);
+        public Thread T_Seven;
+        static public int start { get; set; }
+        static public int end;
+        static public int count;
+        static public List<int> vs;
+        static public List<int> prime;
+        static public List<int> seven;
+        static private void MethodSeven()
+        {
+            seven.Clear();
+            foreach (var item in vs)
+            {
+                if (item % 10 == 7)
+                    seven.Add(item);
+            }
+        }
+        static private void MethodPrimeNumber()
+        {
+            prime.Clear();
+            foreach (var item in GetList())
+            {
+                bool b = true;
+                for (int j = 2; j < item; j++)
+                {
+                    if (item % j == 0 & item % 1 == 0)
+                    {
+                        b = false;
+                    }
+                }
+                if (b) prime.Add(item);
+            }
+        }
+        static private void MethodRandomNum()
+        {
+            vs.Clear();
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                if (start < end)
+                {
+                    vs.Add(random.Next(start, end));
+                }
+                else
+                {
+                    vs.Add(random.Next(end, start));
+                }
+            }
+
+        }
+        public void SaveList()
+        {
+            try
+            {XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<int>));
+            using (FileStream fs = new FileStream("listNumbers.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, vs);
+                MessageBox.Show("Object has been serialized");
+            }
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        static public List<int> GetList()
+        {
+            try
+            {
+               List<int> _vs = new List<int>();  
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<int>));
+            using (FileStream fs = new FileStream("listNumbers.xml", FileMode.OpenOrCreate))
+            {
+                _vs = (List<int>)xmlSerializer.Deserialize(fs);
+                MessageBox.Show("Object has been deserialized");
+            }
+            return _vs;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+    }
+}
